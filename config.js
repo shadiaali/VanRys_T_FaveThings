db43ed36793197af756b8708fe906665544779bc
Van Rys <tvanrys@tss-hr420-dmpd.fanshawec.ca>
2019-03-21 17:03:30 -0400

    finished HW example

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: "root",
+    password: "root", // leave this blank for windows
+    port: 8889, // for windows / linux port is 3306
+    host: "localhost",
+    database: "marvelcharacters"
+}
+
+module.exports = config;
